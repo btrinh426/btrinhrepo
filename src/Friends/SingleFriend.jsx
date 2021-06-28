55b9f3e3514ba06f5217d36e0ed2683495b2e7bf
Brian Trinh <86210145+btrinh426@users.noreply.github.com>
2021-06-28 16:21:47 -0700

    Initialize ScottMatsuda/20210302-205113

---
+++ src/Friends/SingleFriend.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { Button, Card, CardImg, CardText, CardBody, CardTitle } from "reactstrap";
+
+function SingleFriend(props) {
+
+  const oneFriend = props.singleFriend;
+  
+  function onEditClick() {
+    console.log('onEditClick');
+  };
+
+  function onDeleteClick() {
+    console.log('onDeleteClick');
+  };
+
+    return (
+      <div className="container" key={oneFriend.id}>
+        <div className="row">
+          <Card className="col-md-3">
+            <CardImg
+              top
+              width="100%"
+              src={oneFriend.primaryImage.imageUrl}
+              alt="Card image cap"
+            />
+            <CardBody>
+              <CardTitle tag="h5">{oneFriend.title}</CardTitle>
+              <CardText>{oneFriend.summary}</CardText>
+              <Button id={oneFriend.id} className="btn btn-primary" onClick={onEditClick}>Edit</Button>
+              <Button id={oneFriend.id} className="btn btn-primary" onClick={onDeleteClick}>Delete</Button>
+            </CardBody>
+          </Card>
+        </div>
+      </div>
+    );
+};
+
+// export default SingleFriend;
+export default React.memo(SingleFriend);
