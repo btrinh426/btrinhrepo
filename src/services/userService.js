82220d805fff04d23b3bac1b1fd3e005cbbf34f1
Brian Trinh <86210145+btrinh426@users.noreply.github.com>
2021-06-28 16:22:33 -0700

    Initialize JLO727/20210323-211742

---
+++ src/services/userService.js
@@ -0,0 +1,44 @@
+import axios from "axios";
+
+let logIn = payload => {
+    const config = {
+        method: "_PICK_A_HTPP_METHOD",
+        url: "_A_URL_GOES_HERE",
+        data: payload,
+        withCredentials: true,
+        crossdomain: true,
+        headers: { "Content-Type": "application/json" },
+    };
+
+    return axios(config);
+};
+
+let register = (payload, onSuccess, onError) => {
+    const config = {
+        method: "_PICK_A_HTPP_METHOD_FOR_THIS_ENDPOINT",
+        url: "_AnOther_URL_GOES_HERE",
+        data: payload,
+        withCredentials: true,
+        crossdomain: true,
+        headers: { "Content-Type": "application/json" },
+    };
+
+    return axios(config);
+};
+
+let logout = (onSuccess, onError) => {
+    const config = {
+        method: "_PICK_A_HTPP_METHOD_FOR_THIS_ENDPOINT",
+        url: "_AnOther_URL_GOES_HERE",
+        withCredentials: true,
+        crossdomain: true,
+        headers: { "Content-Type": "application/json" },
+    };
+
+    return axios(config);
+};
+
+export { logIn, register, logout }; // export all your calls here
+
+// if you had three functions to export
+// export { logIn, register, thirdFunction }
