08a50c3a97432892bc91d33c506f64f37e0242bc
Brian Trinh <86210145+btrinh426@users.noreply.github.com>
2021-06-28 16:20:30 -0700

    Initialize nitrospaz/20210201-203902

---
+++ src/Components/Registration.jsx
@@ -0,0 +1,187 @@
+import React, { Component } from "react";
+import { toast } from "react-toastify";
+import { usersService } from "../services/userService";
+
+class Registration extends Component {
+  state = {
+    registerFormData: {
+      firstName: "",
+      lastName: "",
+      email: "",
+      avatarUrl: "",
+      password: "",
+      passwordConfirm: "",
+      tenantId: "U01JYBJ1M51",
+    },
+  };
+
+  onSubmitClick = (e) => {
+    e.preventDefault();
+    // console.log("submit clicked");
+    // console.log(e.currentTarget);
+    // "tenantId": "string" is required
+    // let testData = {
+    //   firstName: "string",
+    //   lastName: "cheese",
+    //   email: "user@example.com3",
+    //   password: "Sabio123$%",
+    //   passwordConfirm: "Sabio123$%",
+    //   avatarUrl:
+    //     "https://www.nasa.gov/sites/default/files/styles/full_width_feature/public/thumbnails/image/nhq202101160007_0.jpg",
+    //   tenantId: "string",
+    // };
+    //201 success
+    usersService
+      .register(this.state.registerFormData)
+      .then(this.onSubmitSuccess)
+      .catch(this.onSubmitError);
+  };
+
+  onSubmitSuccess = (response) => {
+    console.log({ good: response });
+    toast.success("Success! You are now registered.");
+    //should call the login function
+    //on success redirect to /Home
+    //on error redirect to /Login
+  };
+
+  onSubmitError = (response) => {
+    console.warn({ error: response });
+    toast.error(
+      "Something went wrong. Please check your inputs and try again."
+    );
+  };
+
+  onFormFieldChange = (e) => {
+    let currentTarget = e.currentTarget; //information on field being filled out
+    console.log(currentTarget);
+    let newValue = currentTarget.value; //new value that was input into the field
+    console.log(newValue);
+    let inputName = currentTarget.name; //name of field
+    console.log(inputName);
+
+    this.setState(() => {
+      let registerFormData = { ...this.state.registerFormData }; //make copy of object of current values
+      registerFormData[inputName] = newValue; //add new value
+      return { registerFormData }; //return object with 1 new value changed
+    });
+  };
+
+  render() {
+    return (
+      <div className="registration">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-3"></div>
+            <div className="col-md-8">
+              <h1 align="center-text">Registration</h1>
+              <form className="needs-validation" id="registrationForm">
+                <div className="form-row">
+                  <div className="col-md-8 mb-3">
+                    <label htmlFor="firstName">First Name</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      id="firstName"
+                      placeholder="First name"
+                      onChange={this.onFormFieldChange}
+                      value={this.state.registerFormData.firstName}
+                      name="firstName"
+                    ></input>
+                  </div>
+                  <div className="col-md-8 mb-3">
+                    <label htmlFor="lastName">Last name</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      id="lastName"
+                      placeholder="Last name"
+                      onChange={this.onFormFieldChange}
+                      value={this.state.registerFormData.lastName}
+                      name="lastName"
+                    ></input>
+                  </div>
+                </div>
+                <div className="form-row">
+                  <div className="col-md-8 mb-3">
+                    <label>Avatar URL</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      id="avatarUrl"
+                      placeholder="URL"
+                      onChange={this.onFormFieldChange}
+                      value={this.state.registerFormData.avatarUrl}
+                      name="avatarUrl"
+                    ></input>
+                  </div>
+                  <div className="col-md-8 mb-3">
+                    <label>Email</label>
+                    <input
+                      type="email"
+                      className="form-control"
+                      id="email"
+                      placeholder="email"
+                      onChange={this.onFormFieldChange}
+                      value={this.state.registerFormData.email}
+                      name="email"
+                    ></input>
+                  </div>
+                </div>
+                <div className="form-row">
+                  <div className="col-md-8 mb-3">
+                    <label htmlFor="password">Password</label>
+                    <input
+                      type="password"
+                      id="password"
+                      placeholder="Password"
+                      className="form-control"
+                      aria-describedby="passwordHelpBlock"
+                      onChange={this.onFormFieldChange}
+                      value={this.state.registerFormData.password}
+                      name="password"
+                    ></input>
+                    <small
+                      id="passwordHelpBlock"
+                      className="form-text text-muted"
+                    >
+                      Make it a good one.
+                    </small>
+                  </div>
+                  <div className="col-md-8 mb-3">
+                    <label htmlFor="confirmPass">Confirm Password</label>
+                    <input
+                      type="password"
+                      id="passwordConfirm"
+                      placeholder="Password"
+                      className="form-control"
+                      aria-describedby="passwordHelpBlock"
+                      onChange={this.onFormFieldChange}
+                      value={this.state.registerFormData.passwordConfirm}
+                      name="passwordConfirm"
+                    ></input>
+                    <small
+                      id="passwordHelpBlock"
+                      className="form-text text-muted"
+                    >
+                      Passwords Must Match
+                    </small>
+                  </div>
+                </div>
+                <button
+                  className="btn btn-primary"
+                  type="submit"
+                  onClick={this.onSubmitClick}
+                >
+                  Submit form
+                </button>
+              </form>
+            </div>
+            <div className="col-md-2"></div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+export default Registration;
---
+++ src/Components/SideBar.jsx
@@ -0,0 +1,122 @@
+import React, { Component } from "react";
+import { usersService } from "../services/userService";
+import { toast } from "react-toastify";
+
+class SideBar extends Component {
+  state = {
+    // currentUserData: {
+    //   id: "",
+    //   firstName: "Default firstName",
+    //   email: "Default email",
+    //   avatarUrl:
+    //     "https://image.shutterstock.com/image-vector/universal-blank-profile-picture-avatar-600w-1654275940.jpg",
+    // },
+  };
+
+  onBtnClick = (e) => {
+    console.log("I was clicked", e.currentTarget.id);
+  };
+
+  onLogoutClicked = (e) => {
+    console.log("onLogoutClicked");
+    usersService
+      .userLogout()
+      .then(this.onLogoutSuccess)
+      .catch(this.onLogoutError);
+  };
+
+  onLogoutSuccess = (response) => {
+    console.log({ good: response });
+    toast.success("You have successfully logged out.");
+    //redirect to login page
+    this.props.history.push("/Login");
+  };
+  onLogoutError = (response) => {
+    console.log({ error: response });
+    toast.error("Something went wrong, please try again.");
+  };
+
+  onFriendsClicked = () => {
+    console.log("Friends Clicked");
+    this.props.history.push("/Friends");
+  };
+
+  render() {
+    return (
+      <div className="col-12 col-md-3 col-xl-2 bd-sidebar text-center">
+        <nav className=" bd-links" id="bd-docs-nav">
+          {/* <div className="text-center" id="userAvatar">
+            <img
+              src={this.state.currentUserData.avatarUrl}
+              className="rounded img-thumbnail"
+              alt="user avatar"
+            />
+          </div>
+          <div className="text-center">
+            Logged in as: <p>{this.state.currentUserData.email}</p>
+          </div> */}
+          <div className="list-group" id="sideNav">
+            <button
+              type="button"
+              className="list-group-item list-group-item-action"
+              onClick={this.onFriendsClicked}
+              id="friendsBtn"
+            >
+              Friends
+            </button>
+            <button
+              type="button"
+              className="list-group-item list-group-item-action"
+              onClick={this.onBtnClick}
+              id="eventsBtn"
+            >
+              Events
+            </button>
+            <button
+              type="button"
+              className="list-group-item list-group-item-action"
+              onClick={this.onBtnClick}
+              id="blogsBtn"
+            >
+              Blogs
+            </button>
+            <button
+              type="button"
+              className="list-group-item list-group-item-action"
+              onClick={this.onBtnClick}
+              id="jobsBtn"
+            >
+              Jobs
+            </button>
+            <button
+              type="button"
+              className="list-group-item list-group-item-action"
+              onClick={this.onBtnClick}
+              id="techCompaniesBtn"
+            >
+              Tech Companies
+            </button>
+            <button
+              type="button"
+              className="list-group-item list-group-item-action"
+              onClick={this.onBtnClick}
+              id="registerBtn"
+            >
+              Register
+            </button>
+            <button
+              type="button"
+              className="list-group-item list-group-item-action"
+              onClick={this.onLogoutClicked}
+              id="logoutBtn"
+              // disabled
+            >
+              Logout
+            </button>
+          </div>
+        </nav>
+      </div>
+    );
+  }
+}
+export default SideBar;
