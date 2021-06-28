47aef0be4e18eddb040e21a4b6afad3133de8a86
Brian Trinh <86210145+btrinh426@users.noreply.github.com>
2021-06-28 16:23:10 -0700

    Initialize nexecute/20210414-115158

---
+++ src/navagation/FooterComponent.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+
+
+class FooterComponent extends React.Component
+{
+    render()
+    {
+        return(
+            <React.Fragment>
+                <footer className="container">
+          <p>&copy; Sabio 2019-2020</p>
+        </footer>
+            </React.Fragment>
+        )
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export default FooterComponent;
