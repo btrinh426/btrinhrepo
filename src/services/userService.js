8e62cb1c158edec99c4dd99bd7341c2b88d35aa1
Brian Trinh <86210145+btrinh426@users.noreply.github.com>
2021-06-28 16:22:28 -0700

    Initialize jpark2111/20210309-230409

---
+++ src/services/userService.js
@@ -0,0 +1,52 @@
+import axios from "axios";
+
+let endpointUser = "https://api.remotebootcamp.dev/api/users";
+let userLogin = (payload) => {
+  const config = {
+    method: "POST",
+    url: endpointUser + "/login",
+    data: payload,
+    crossdomain: true,
+    headers: { "Content-Type": "application/json" },
+  };
+  return axios(config);
+};
+let getUserLogout = () => {
+  const config = {
+    method: "GET",
+    url: endpointUser + "/logout",
+    crossdomain: true,
+    headers: { "Content-Type": "application/json" },
+  };
+  return axios(config);
+};
+let add = (payload) => {
+  const config = {
+    method: "POST",
+    url: endpointUser + "/register",
+    data: payload,
+    crossdomain: true,
+    headers: { "Content-Type": "application/json" },
+  };
+  return axios(config);
+};
+let getCurrentUser = () => {
+  const config = {
+    method: "GET",
+    url: endpointUser + "/current",
+    crossdomain: true,
+    headers: { "Content-Type": "application/json" },
+  };
+  return axios(config);
+};
+let getUserNameById = (id) => {
+  const config = {
+    method: "GET",
+    url: endpointUser + "/" + id,
+    crossdomain: true,
+    headers: { "Content-Type": "application/json" },
+  };
+  return axios(config);
+};
+
+export { userLogin, getUserLogout, add, getCurrentUser, getUserNameById };
