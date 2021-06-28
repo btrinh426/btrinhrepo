12b4b09e0bd609243ed6680be5505196a6469bd5
Brian Trinh <86210145+btrinh426@users.noreply.github.com>
2021-06-28 16:23:34 -0700

    Initialize geduardo21/20210424-140127

---
+++ src/App.jsx
@@ -0,0 +1,79 @@
+import React, { Component } from "react";
+import SiteNav from "../src/Content/SiteNav";
+import "./App.css";
+//import SiteNav from "./Content/Footer";
+import Footer from "./Content/Footer";
+import Jumbo from "./Content/Jumbo";
+import Content from "./Content/Content";
+import { BrowserRouter, Route } from "react-router-dom";
+import Register from "./components/Register";
+import Login from "./components/Login";
+import Home from "./components/Home";
+import AddorEdit from "./components/AddorEdit"
+import Friends from "./components/Friends"
+
+
+
+class App extends Component {
+  // componentDidMount() {
+  //   const payload = {
+  //     email: "eduardo@sabiola.com",
+  //     password: "Gomez2021!",
+  //     tenantId: "U01LFUP9KB4",
+  //   };
+   
+
+  //   login(payload)
+  //   .then(this.onLoginSuccess)
+  //   .catch(this.onLoginError);
+  // }
+ 
+   
+  
+  // onLoginSuccess = (Response) => console.log(Response);
+  // onLoginError = (errResponse) => console.warn(errResponse);
+  
+  render() {
+    return (
+      <BrowserRouter>
+        <React.Fragment>
+          <div>
+            <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+            
+              <SiteNav></SiteNav>
+              
+            </nav>
+          </div>
+
+          <main className="nav" role="main">
+            
+           
+          
+            
+            <Route path="/Jumbo" exact={true} component={Jumbo} />
+            {/* <Jumbo></Jumbo>  */}
+            <div className="container form">
+            <Route path="/register" exact={true} component={Register} />
+            <Route path="/login" exact= {true} component={Login}/>
+            <Route path="/home" exact={true} component={Home}/>
+            <Route path= "/addoredit" exact={true} component={AddorEdit}/>      
+            </div>
+            <div className="container friends">
+            <Route path="/friends" exact={true} component={Friends}/>
+            
+            </div>
+            <div>
+              <Route path="/Content" exact={true} component={Content} />
+              {/* <Content></Content> */}
+            </div>
+          </main>
+      <div className="footer">
+          <Footer />
+          </div>
+        </React.Fragment>
+      </BrowserRouter>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Friends.jsx
@@ -0,0 +1,88 @@
+import React, { Component } from "react";
+import {getFriends,deleteFriend} from "../services/appService"
+import SingleFriend from "./SingleFriend";
+
+class Friends extends Component{
+
+    componentDidMount(){
+        getFriends()
+        .then(this.onGetFriendsSuccess)
+        .catch(this.onGetFriendsError)
+
+       
+       
+    }
+
+    state ={
+        friend:{
+            "title":""
+            ,"bio":""
+            ,"summary":""
+            ,"headline":""
+            ,"slug":""
+            ,"statusId":""
+            ,"primaryImage":""
+        }
+    }
+
+    onGetFriendsSuccess = (response) => 
+    {
+        let friends = response.data.item.pagedItems;
+        this.setState((preState)=>
+        {
+            return {mappedFriends:friends.map(this.mapFriend)};
+        })
+        
+        console.log(friends);
+    };
+    onGetFriendsError = (response) => console.warn(response);
+
+    onFriendDeleteClick = (e) => 
+    {
+        // e.preventDefault();
+        console.log(e.id)
+        deleteFriend(e.id)
+        .then(this.onDeleteFriendSuccess)
+        .catch(this.onDeleteFriendError)
+    };
+
+    onDeleteFriendSuccess = (response) => console.log(response);
+    onDeleteFriendError = (err) => console.error(err);
+
+    mapFriend = (friend) =>
+    {
+        return(
+            <React.Fragment key={friend.id}>
+                <SingleFriend friend ={friend}
+                onClick={this.onFriendDeleteClick}></SingleFriend>
+           
+          
+              </React.Fragment>
+        )
+    }
+
+
+    render(){
+        return(
+            <React.Fragment>
+               
+        {/* <nav className="navbar navbar-expand-lg navbar-light bg-light"/>
+            <div className="container-flu   id"/>
+                <a className="navbar-brand" href=" ">Friends</a>
+                <a href="addEditFriend.html" id="addFriend" className="btn btn-outline-success" type="submit">+Friend</a>
+                <form className="d-flex"/>
+                    <input className="form-control me-2" type="search" placeholder="Search friends" aria-label="Search"/>
+                    <button className="btn btn-outline-success" type="submit">Search</button> */}
+
+                  
+                    <div className="card w-20">
+                        {this.state.mappedFriends}
+                    </div>
+                    
+   
+            </React.Fragment>
+        )
+    }
+};
+
+export default Friends;
