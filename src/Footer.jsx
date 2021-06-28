1d2659b990c2091998a5538f97702833e1d21165
Brian Trinh <86210145+btrinh426@users.noreply.github.com>
2021-06-28 16:20:14 -0700

    Initialize ZacharyFinn/20210127-210156

---
+++ src/Footer.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+class Footer extends React.Component
+{
+    render()
+    {
+        return(
+            <React.Fragment>
+                <footer className="container fixed-bottom">
+                    <p>&copy; Sabio 2021</p>
+                </footer>
+            </React.Fragment>
+        )
+    }
+}
+
+export default Footer;
