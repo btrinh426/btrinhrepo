19725d03bded82c79e53c58af3bd48323ef89ef2
Brian Trinh <86210145+btrinh426@users.noreply.github.com>
2021-06-28 16:22:12 -0700

    Initialize kdashiell2020/20210309-191917

---
+++ src/services/userService.js
@@ -0,0 +1,37 @@
+import axios from "axios";
+
+let login = payload => {
+    const config = {
+        method: 'POST',
+        url: 'https://api.remotebootcamp.dev/api/users/login',
+        data: payload,
+        withCredentials: true,
+        crossdomain: true,
+        headers: {'Content-Type':'application/json'}
+    }
+    return axios(config)
+}
+
+let logout = () => {
+    const config = {
+        method: 'GET',
+        url: 'https://api.remotebootcamp.dev/api/users/logout',
+    }
+    return axios(config)
+}
+
+let register = (payload) => {
+    console.log(payload)
+    const config = {
+        method: 'Post',
+        url: 'https://api.remotebootcamp.dev/api/users/register',
+        data: payload,
+        crossdomaim: true,
+        withCredentials: true,
+        headers: { 'Content-Type': 'application/json' }
+    }
+    return axios(config)
+}
+
+
+export { logout, login, register }
